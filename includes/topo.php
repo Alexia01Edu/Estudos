65069f773ba1e8cfc031c0720c128ef02a424bea
alexia <alexiaedu012004@gmail.com>
2022-12-13 17:09:36 -0300

    in

--- includes/topo.php
+++ includes/topo.php
@@ -1,8 +1,73 @@
 <header>
-      <div class="barra_menu">
+    <div class="topo">
+        <div class="barra_menu">
           <div class="imagem-topo"></div>
-      </div>
-      <div class="menu-itens">
-          
-      </div>  
+          <div class="bar">
+            <div class="bar-img">
+                <form action="">
+                    <label for="" class="procure"></label>
+                    <input type="text" class="procurar">
+                </form>
+            </div>
+          </div>
+        </div>
+        <div class="menu-itens">
+            <nav class="links-menu">
+                <ul class="lista-topo">
+                    <li class="materias">
+                        <a href="" class="links">Matemática</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Língua Portuguesa</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Inglês</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Redes de Computadores</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Biologia</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Química</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Física</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Educação física</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">História</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links">Geografia</a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links"></a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links"></a>
+                        <div class="conteudo"></div>
+                    </li>
+                    <li class="materias">
+                        <a href="" class="links"></a>
+                        <div class="conteudo"></div>
+                    </li>
+                </ul>
+            </nav>
+        </div>  
+    </div>
 </header>
